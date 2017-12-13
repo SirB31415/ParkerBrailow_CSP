fa4a4d4e3910f9ac5691425c1abeffa507581354
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-12-13 12:00:00 -0700

    Added sounds, images, and stuff to the model file.

--- ParkerBrailow_CSP/Controller/CreativityViewController.swift
+++ ParkerBrailow_CSP/Controller/CreativityViewController.swift
@@ -75,12 +75,12 @@ public class CreativityViewController: UICollectionViewController, UICollectionV
         return artSelection.count
     }
     
-    public func collectionView(_ collectionView: UICollectionView,
-                                        cellForItemAt indexPath: IndexPath) -> UICollectionView
+    override public func collectionView(_ collectionView: UICollectionView,
+                                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
-        let artCell = collectionView.dequequeReusableCell(withReuseIdentifier: reuseIdentifier, for indexPath) as! ArtCell
+        let artCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ArtCell
         
-        artCell.backgroundColor = .purple
+        artCell.backgroundColor = .blue
         artCell.imageView.image = artSelection[indexPath.row]
         artCell.imageName.text = "My Art"
         
--- ParkerBrailow_CSP/View/DrawingView.swift
+++ ParkerBrailow_CSP/View/DrawingView.swift
@@ -18,18 +18,63 @@ public class DrawingView: UIView
         drawStickFigure().stroke()
         drawTurtle()
         drawHappyTree()
+        drawStickyFigure().stroke()
+        drawStickyyFigure().stroke()
+        drawHeart()
     }
-    
+    public func drawStickyFigure() -> UIBezierPath
+    {
+        let stickyFigure = UIBezierPath()
+        
+        UIColor.blue.setStroke()
+        stickyFigure.lineWidth = 3.0
+        
+        stickyFigure.addArc(withCenter: CGPoint(x: 280, y: 180),
+                           radius: CGFloat(20),
+                           startAngle: CGFloat(0),
+                           endAngle:CGFloat(2) * CGFloat.pi,
+                           clockwise: true)
+        stickyFigure.move(to: CGPoint(x: 280, y: 200))
+        stickyFigure.addLine(to: CGPoint(x: 280, y:270))
+        stickyFigure.move(to: CGPoint(x: 260, y: 240))
+        stickyFigure.addLine(to: CGPoint(x: 300, y: 240))
+        stickyFigure.move(to: CGPoint(x: 280, y: 270))
+        stickyFigure.addLine(to: CGPoint(x: 260, y: 300))
+        stickyFigure.move(to: CGPoint(x: 280, y: 270))
+        stickyFigure.addLine(to: CGPoint(x: 300, y: 300))
+        return stickyFigure
+    }
+    public func drawStickyyFigure() -> UIBezierPath
+    {
+        let stickyyFigure = UIBezierPath()
+        
+        UIColor.red.setStroke()
+        stickyyFigure.lineWidth = 3.0
+        stickyyFigure.addArc(withCenter: CGPoint(x: 240, y: 180),
+                           radius: CGFloat(20),
+                           startAngle: CGFloat(0),
+                           endAngle:CGFloat(2) * CGFloat.pi,
+                           clockwise: true)
+        stickyyFigure.move(to: CGPoint(x: 240, y: 200))
+        stickyyFigure.addLine(to: CGPoint(x: 240, y:270))
+        stickyyFigure.move(to: CGPoint(x: 220, y: 240))
+        stickyyFigure.addLine(to: CGPoint(x: 260, y: 240))
+        stickyyFigure.move(to: CGPoint(x: 240, y: 270))
+        stickyyFigure.addLine(to: CGPoint(x: 220, y: 300))
+        stickyyFigure.move(to: CGPoint(x: 240, y: 270))
+        stickyyFigure.addLine(to: CGPoint(x: 260, y: 300))
+        return stickyyFigure
+    }
     public func drawStickFigure() -> UIBezierPath
     {
         let stickFigure = UIBezierPath()
         
-        UIColor.magenta.setStroke()
+        UIColor.green.setStroke()
         stickFigure.lineWidth = 3.0
         
-        stickFigure.addArc(withCenter: CGPoint(x: 200, y: 200),
+        stickFigure.addArc(withCenter: CGPoint(x: 200, y: 180),
                            radius: CGFloat(20),
-                           startAngle: CGFloat(6),
+                           startAngle: CGFloat(0),
                            endAngle:CGFloat(2) * CGFloat.pi,
                            clockwise: true)
         stickFigure.move(to: CGPoint(x: 200, y: 200))
@@ -42,6 +87,20 @@ public class DrawingView: UIView
         stickFigure.addLine(to: CGPoint(x: 220, y: 300))
         return stickFigure
     }
+    public func drawHeart() -> Void
+    {
+        let heart = UIBezierPath()
+        
+        heart.move(to: CGPoint(x: 240, y: 0))
+        heart.addLine(to: CGPoint(x: 300, y: 150))
+        heart.addLine(to: CGPoint(x: 180, y: 150))
+        heart.close()
+        UIColor(patternImage: UIImage(named: "chanukahHeart")!).setFill()
+        UIColor.yellow.setStroke()
+        heart.lineWidth = 2.0
+        heart.fill()
+        heart.stroke()
+    }
     public func drawHappyTree() -> Void
     {
         let dance = UIBezierPath()
@@ -50,8 +109,8 @@ public class DrawingView: UIView
         dance.addLine(to: CGPoint(x: 120, y: 150))
         dance.addLine(to: CGPoint(x: 40, y: 150))
         dance.close()
-        UIColor(patternImage: UIImage(named: "tenor")!).setFill()
-        UIColor.magenta.setStroke()
+        UIColor(patternImage: UIImage(named: "binary")!).setFill()
+        UIColor.red.setStroke()
         dance.lineWidth = 2.0
         dance.fill()
         dance.stroke()
