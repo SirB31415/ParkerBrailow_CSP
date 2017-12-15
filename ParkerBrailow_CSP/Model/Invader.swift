ae83e0bc14de03f4f9680edb37db671044aeae63
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-12-15 11:48:39 -0700

    Added the ability for the invader to move and switch directions.

--- ParkerBrailow_CSP/Model/Invader.swift
+++ ParkerBrailow_CSP/Model/Invader.swift
@@ -21,7 +21,12 @@ public class Invader: SKSpriteNode
         let texture = SKTexture(imageNamed: "enemy1")
         super.init(texture: texture, color: SKColor.clear, size: texture.size())
         self.name = "invader"
-    
+        
+        self.physicsBody = SKPhysicsBody(texture: self.texture!, size: self.size)
+        self.physicsBody?.isDynamic = true
+        self.physicsBody?.usesPreciseCollisionDetection = false
+        self.physicsBody?.categoryBitMask = CollisionCategories.PlayerBullet | CollisionCategories.Player
+        self.physicsBody?.collisionBitMask = CollisionCategories.EdgeBody
     }
     // Required init must be provided by subclass
     required public init?(coder aDecoder: NSCoder)
