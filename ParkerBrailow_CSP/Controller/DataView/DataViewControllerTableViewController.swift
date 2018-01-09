64d54fc3c4846d905c0e3542883ea01e9bc8b13e
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2018-01-09 13:50:24 -0700

    I finished the data screen.

--- ParkerBrailow_CSP/Controller/DataView/DataViewControllerTableViewController.swift
+++ ParkerBrailow_CSP/Controller/DataView/DataViewControllerTableViewController.swift
@@ -19,33 +19,48 @@ class DataViewControllerTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    lazy var bucketList : [BucketItem] =
+        {
+            return loadBucketListFromFile()
+        }()
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        if let filePath = Bundle.main.url(forResource: "bucket", withExtension: "csv")
+        {
+            do
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0], author: item[1]))
+                }
+            }
+            catch
+            {
+                print("File read error")
+            }
+        }
+        return items
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return bucketList.count
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier", for: indexPath) as! BucketItemCellTableViewCell
+        
+        currentCell.bucketItem = bucketList[indexPath.row]
+        
+        return currentCell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
---
+++ ParkerBrailow_CSP/Model/BucketItem.swift
@@ -0,0 +1,21 @@
+//
+//  BucketItem.swift
+//  ParkerBrailow_CSP
+//
+//  Created by Brailow, Parker on 1/9/18.
+//  Copyright © 2018 Brailow Inc. All rights reserved.
+//
+
+import Foundation
+
+public class BucketItem
+{
+    let itemContents : String
+    let itemAuthor : String
+    
+    init(contents : String, author : String)
+    {
+        itemContents = contents
+        itemAuthor = author
+    }
+}
